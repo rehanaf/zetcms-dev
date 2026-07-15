8e66bd3ac0d8b5d7720441fdcfcd19019c896b33
rehanaf <rehanalfaqih287@gmail.com>
2026-07-15 16:17:43 +0700

    fix: correct actions namespace in ThemesTable for filament 5

--- app/Filament/Resources/Themes/Tables/ThemesTable.php
+++ app/Filament/Resources/Themes/Tables/ThemesTable.php
@@ -2,10 +2,10 @@
 
 namespace App\Filament\Resources\Themes\Tables;
 
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\EditAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
